4e11696cff976d9f3b2932d567869f218558c2fb
pushpa8285 <pushpa82855@gmail.com>
2025-10-31 03:07:09 +0000

    js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 144553;
+let accountEmail = "pusspa85855@gmail.com";
+console.log(accountEmail)
+
+const myName ="Pushpa Kumai"
+console.log(myName)
+
+let MyClass = "12th"
+MyClass= "graduation"
+console.log(MyClass)
